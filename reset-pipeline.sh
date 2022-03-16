51cf9526c32fa71d0f55945eef89d9dd8e18d1da
Imran Asif <imran@pop-os.localdomain>
2022-03-16 17:00:06 -0600

    docker image.

--- reset-pipeline.sh
+++ reset-pipeline.sh
@@ -3,4 +3,5 @@
 
 fly -t ps destroy-pipeline  -p terraform-fmt-validate 
 
-fly -t ps set-pipeline -c pipeline_terraform.yml -p terraform-fmt-validate 
+fly -t ps set-pipeline -c pipeline_terraform.yml -p terraform-fmt-validate
+fly -t ps unpause-pipeline -p terraform-fmt-validate
